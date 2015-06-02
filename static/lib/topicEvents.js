e0adfbe698ed205d475c63e7ef9a7bb0653bbaec
Raphael Beer <raphaelbeer030@gmail.com>
2015-06-02 04:07:16 +0200

    rearrange events on infinite scroll

--- static/lib/topicEvents.js
+++ static/lib/topicEvents.js
@@ -5,14 +5,23 @@ require(['translator'], function(translator) {
 
   var TopicEvents = {
 
+    _firstAnswerId: 0,
+    _headEvents: [],
+    _tailEvents: [],
+
     init: function() {
       var tid = ajaxify.variables.get('topic_id');
+      var firstId = document.querySelectorAll('[component="post"]').
+                    item(1).dataset.index;
+      TopicEvents._firstAnswerId = Number.parseInt(firstId);
+
       TopicEvents.getState(tid, function(hidden) {
         TopicEvents.setEventTool(hidden);
         if (!hidden) {
           TopicEvents.getTopicEvents({tid: tid});
         }
       });
+
       $('.toggle-events').on('click', function(evt) {
         // topic tool DOM
         var ttDOM = $('.toggle-events').children()[0];
@@ -81,10 +90,63 @@ require(['translator'], function(translator) {
       TopicEvents.addTopicEvent(data);
     },
 
-    clearTopicEvents: function() {
-      var eventBlocks = document.getElementsByClassName('topic-events-block');
-      for (var i = eventBlocks.length - 1; i >= 0; i--) {
-        eventBlocks[i].parentNode.removeChild(eventBlocks[i]);
+    placeTopicEvent: function(posts, eventElement, tstamp) {
+
+      var nextTstamp = 0;
+
+      if (posts instanceof NodeList) {
+        posts = Array.prototype.slice.call(posts);
+      }
+
+      // if only one post || event is older than 1st answer
+      if (posts.length === 1 || tstamp < posts[1].dataset.timestamp) {
+        TopicEvents._headEvents.push(eventElement);
+      }
+      // event is newer than last post
+      else if (tstamp > posts[posts.length - 1].dataset.timestamp) {
+        TopicEvents._tailEvents.push(eventElement);
+      }
+
+      for (var pIdx = 0; pIdx <= posts.length - 1; pIdx++) {
+        if (pIdx != posts.length - 1) {
+          nextTstamp = posts[pIdx + 1].dataset.timestamp;
+        } else {
+          nextTstamp = tstamp + 1;
+        }
+
+        if (posts[pIdx].dataset.timestamp < tstamp &&
+            nextTstamp > tstamp) {
+
+          var post = posts[pIdx];
+          var possEvent = post.nextElementSibling;
+
+          // nextElementSibling is null, so post was the last post
+          // withour any events attached
+          if (possEvent === null) {
+            post.insertAdjacentElement('afterend', eventElement);
+            return;
+          }
+
+          while (possEvent.getAttribute('component') === 'topic/event') {
+
+            if (possEvent.dataset.timestamp > tstamp) {
+              possEvent.insertAdjacentElement('beforebegin', eventElement);
+              return;
+            }
+
+            // iterate
+            if (possEvent.nextElementSibling != null) {
+              possEvent = possEvent.nextElementSibling;
+            } else {
+              possEvent.insertAdjacentElement('afterend', eventElement);
+              return;
+            }
+          }
+          // if routes in loop didnt return,
+          // possEvent is the next not-event Element in posts <ul>
+          possEvent.insertAdjacentElement('beforebegin', eventElement);
+          return;
+        }
       }
     },
 
@@ -93,45 +155,19 @@ require(['translator'], function(translator) {
         translator.translate(tpl, function(content) {
 
           var posts = document.querySelectorAll('li[component=post]');
-          var nextTstamp = 0;
-          var newEvtRow = $(content);
-          newEvtRow.find('.timeago').timeago();
+          var newEventElement = $(content);
+          newEventElement.find('.timeago').timeago();
 
-          for (var pIdx = 0; pIdx <= posts.length - 1; pIdx++) {
-            if (pIdx != posts.length - 1) {
-              nextTstamp = posts.item(pIdx + 1).dataset.timestamp;
-            } else {
-              nextTstamp = data.tstamp + 1;
-            }
-
-            if (posts.item(pIdx).dataset.timestamp < data.tstamp &&
-                nextTstamp > data.tstamp) {
-
-              var post = posts.item(pIdx);
-              var possEvent = (post.nextElementSibling != null) ?
-                                post.nextElementSibling : post;
-
-              while (possEvent.getAttribute('component') === 'topic/event') {
-
-                if (possEvent.dataset.timestamp > data.tstamp) {
-                  possEvent.insertAdjacentElement('beforebegin', newEvtRow[0]);
-                  return;
-                }
-
-                // iterate
-                if (possEvent.nextElementSibling != null) {
-                  possEvent = possEvent.nextElementSibling;
-                } else {
-                  possEvent.insertAdjacentElement('afterend', newEvtRow[0]);
-                  return;
-                }
-              }
-              post.insertAdjacentElement('afterend', newEvtRow[0]);
-              return;
-            }
-          }
+          TopicEvents.placeTopicEvent(posts, newEventElement[0], data.tstamp);
         });
       });
+    },
+
+    clearTopicEvents: function() {
+      var eventBlocks = document.getElementsByClassName('topic-events-block');
+      for (var i = eventBlocks.length - 1; i >= 0; i--) {
+        eventBlocks[i].parentNode.removeChild(eventBlocks[i]);
+      }
     },
 
     setEventTool: function(hidden) {
@@ -164,17 +200,39 @@ require(['translator'], function(translator) {
   $(window).on('action:topic.loaded', TopicEvents.init);
   $(window).on('action:posts.loaded', function(evt, data) {
     console.log('postsloaded');
-    /*// new posts are delivered alone && have a CategoryID
-    if (data.posts.length === 1 && data.posts[0].cid) {
-      var selector = 'li[component="post"][data-timestamp="' +
-                     post.timestamp + '"]';
-      var newPost = document.querySelector(selector);
-      if (newPost.nextSibling.className === 'topic-events-block') {
-        var tail = newPost.nextSibling;
-        tail.parentElement.removeChild(tail);
-        newPost.insertAdjacentElement('beforebegin', tail);
+    //new posts are delivered alone && have a CategoryID
+    if (data.posts.length === 1 && data.posts[0].cid) { }
+
+    var posts = [document.querySelector('[component="post"][data-index="0"]')];
+    for (var i = 0; i <= data.posts.length - 1; i++) {
+      var qs = '[component="post"][data-index="' +
+               data.posts[i].index + '"]';
+      posts.push(document.querySelector(qs));
+    }
+
+    var newLastId = data.posts[data.posts.length - 1].index;
+    if (newLastId < TopicEvents._firstAnswerId) {
+      if (TopicEvents._headEvents.length === 0) {
+        return;
       }
-    }*/
+      console.log('going UP');
+      var iterEvents = TopicEvents._headEvents.slice(0);
+      var moveEvents = TopicEvents._headEvents;
+    } else {
+      if (TopicEvents._tailEvents.length === 0) {
+        return;
+      }
+      console.log('going DOWN');
+      var iterEvents = TopicEvents._tailEvents.slice(0);
+      var moveEvents = TopicEvents._tailEvents;
+    }
+    for (var hIdx = 0; hIdx <= iterEvents.length - 1; hIdx++) {
+      var evt = iterEvents[hIdx];
+      evt.parentElement.removeChild(evt);
+      moveEvents.shift();
+      TopicEvents.placeTopicEvent(posts, evt, evt.dataset.timestamp);
+    }
+    return;
   });
 
   socket.on('event:topic_pinned', TopicEvents.getTopicEvents);
